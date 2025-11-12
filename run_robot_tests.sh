96b22391288886db6809a46129c5747c3971e28b
Santeri Silvennoinen <santeri.silvennoinen@helsinki.fi>
2025-11-12 19:12:31 +0200

    test

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -3,25 +3,22 @@ set -x
 echo "Running tests"
 
 export DISPLAY=:99
+export MOZ_HEADLESS=1
 export PATH=/usr/local/bin:/usr/bin:$PATH
 
-# Debug
 which firefox
-firefox --version
+firefox --headless --version
 which geckodriver
 geckodriver --version
 echo $PATH
 ps -ef | grep Xvfb
 
-# Käynnistetään Flask-palvelin taustalle
 poetry run python3 src/index.py &
 echo "started Flask server"
 
-# Odotetaan, että palvelin on valmis
 while [[ "$(curl -s -o /dev/null -w '%{http_code}' localhost:5001)" != "200" ]]; do sleep 1; done
 echo "Flask server is ready"
 
-# Suoritetaan testit Firefoxilla headless-tilassa ja pidemmällä timeoutilla
 poetry run robot --variable BROWSER:firefox --variable HEADLESS:true --variable SELENIUM_TIMEOUT:30 src/tests
 
 status=$?
