cd22ccbd31ec39ca24cfb9e9a3e353b068ba2068
Santeri Silvennoinen <santeri.silvennoinen@helsinki.fi>
2025-11-14 13:20:49 +0200

    test

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-set -x
+
 echo "Running tests"
 
-export DISPLAY=:99
-export MOZ_HEADLESS=1
-export PATH=/usr/local/bin:/usr/bin:$PATH
-
-which firefox
-firefox --headless --version
-which geckodriver
-geckodriver --version
-echo $PATH
-ps -ef | grep Xvfb
-
+# käynnistetään Flask-palvelin taustalle
 poetry run python3 src/index.py &
+
 echo "started Flask server"
 
-while [[ "$(curl -s -o /dev/null -w '%{http_code}' localhost:5001)" != "200" ]]; do sleep 1; done
+# odotetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5001)" != "200" ]];
+  do sleep 1;
+done
+
 echo "Flask server is ready"
 
-poetry run robot --variable BROWSER:firefox --variable HEADLESS:true --variable SELENIUM_TIMEOUT:30 src/tests
+# suoritetaan testit
+poetry run robot --variable HEADLESS:true src/tests
 
 status=$?
+
+# pysäytetään Flask-palvelin portissa 5001
 kill $(lsof -t -i:5001)
+
 exit $status
